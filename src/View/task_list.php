e1136a4e9e5b4ca213832a9471c5591bb6800042
DieselComputer <boss.newbee@mail.ru>
2026-01-13 14:24:41 +1100

    done

--- src/View/task_list.php
+++ src/View/task_list.php
@@ -7,14 +7,14 @@
 </head>
 
 <body>
-    <h1> Список задач</h1>
+    <h2>Список задач</h2>
     <ul>
         <?php foreach ($tasks as $task): ?>
             <li><?= htmlspecialchars($task->getTitle())?></li>
             <?= $task->isCompleted() ? "✔" : "❌"?>
         </li>
         <?php endforeach; ?>
-        <a href="?route=task/add"></a>
+        <a href="?route=task/add">Добавить</a>
     </ul>
 </body>
 
